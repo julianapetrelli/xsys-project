0745f867c6facafb85906238720d0a9def9b7631
juliatangerino <julianatangerino2017@gmail.com>
2021-09-03 01:27:14 -0300

    Meu primeiro commit

---
+++ assets/js/app.js
@@ -0,0 +1,81 @@
+// galery image
+
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+})
+
+
+// comentarios
+
+const content = document.getElementById("content");
+const cardTemplate = document.getElementById("card-template");
+
+loadComments(true);
+
+// botão de comentários
+
+const button = document.getElementById("loadAllComments");
+const buttonArea = document.querySelector(".button-comments");
+
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    loadComments(false);
+});
+
+
+function loadComments(showOnLoad) {
+    database.forEach(function (card) {
+        
+        if (!card.showOnLoad === showOnLoad) {
+            return
+        }
+
+        let cardClone = document.importNode(cardTemplate.content, true);
+
+        cardClone.querySelector("#photo").setAttribute('src', card.photo);
+        cardClone.querySelector("#name").textContent = card.name;
+        cardClone.querySelector("#comment").textContent = card.comment;
+
+        content.appendChild(cardClone);
+    });
+
+    if (!showOnLoad) {
+        buttonArea.classList.add("hidden");
+    }
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    let form = document.getElementById('contact-form');
+    let formData = new FormData(form);
+
+    let formDataObject = {};
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
+    });
+
+    if (formDataObject.email === '' || formDataObject.message === '') {
+        alert('Preencha todos os campos');
+        return;
+    }
+    
+    fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+    }).then(() => {
+        alert('Formulario Enviado com Successo!')
+        form.getElementsByTagName('input')[0].value = '';
+        form.getElementsByTagName('textarea')[0].value = '';
+    }).catch((error) => console.error(error))
+}
+
+document.getElementById("submit").addEventListener("click", handleSubmit);
---
+++ database/database.js
@@ -0,0 +1,58 @@
+const database = [
+    { 
+        "id": 1,
+        "photo": "/assets/img/person-one.jpg",
+        "name": "SAMUEL PIETRO CALDEIRA",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": true
+    },
+    { 
+        "id": 2,
+        "photo": "/assets/img/person-two.jpg",
+        "name": "IGOR NOAH MOREIRA",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": true
+    },
+    { 
+        "id": 3,
+        "photo": "/assets/img/person-three.jpg",
+        "name": "EMANUELLY ISABELLY ASSIS",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": true
+    },
+    { 
+        "id": 4,
+        "photo": "/assets/img/person-four.jpg",
+        "name": "HELENA DUCATI AMARAL",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": true
+    },
+    { 
+        "id": 5,
+        "photo": "/assets/img/person-five.jpg",
+        "name": "GABRIEL TEIXEIRA GOMES",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": false
+    },
+    { 
+        "id": 6,
+        "photo": "/assets/img/person-six.jpg",
+        "name": "HEITOR GOMES AMARAL",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": false
+    },
+    { 
+        "id": 7,
+        "photo": "/assets/img/person-seven.jpg",
+        "name": "ARTHUR AMARAL DUCATI",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": false
+    },
+    { 
+        "id": 8,
+        "photo": "/assets/img/person-eight.jpg",
+        "name": "GIOVANA MACHADO TEIXEIRA",
+        "comment": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit asperiores tempora sit minus cum quos facilis modi ipsum laboriosam.",
+        "showOnLoad": false
+    }
+];
